bf922caf0bd8791b224edeca46cedde42e8f09b9
Gabriel Abarca <gabrielaher27@hotmail.com>
2024-08-03 17:32:42 -0600

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      colors:{
+      bluefc:'#020d73'
+    }},
+  },
+  plugins: [],
+}
+
