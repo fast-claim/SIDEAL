5497ea9e923ae2d0be04fd5ba200cc2488274559
Gabriel Abarca <gabrielaher27@hotmail.com>
2024-08-04 11:59:24 -0600

    Se añade la fuente de letra y los colores que faltaban de la paleta establecida

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,13 @@ module.exports = {
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
+      manrope:['Manrope']
     },
     extend: {
       colors:{
-      bluefc:'#020d73'
+      bluefc:'#020d73',
+      purplefc:'#240048',
+      cyanfc:'00689b'
     }},
   },
   plugins: [],
