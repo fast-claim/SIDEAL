2f2f31b2397cd3861f202f4c9b8f8a79b2bbcf0b
Gabriel Abarca <epoabarca@gmail.com>
2024-10-21 21:41:10 -0600

    cambio en formularios, ahora tienen mas informacion

---
+++ public/js/modalFormularios.js
@@ -0,0 +1,20 @@
+function modalForms(idForm){
+    let titulo=document.getElementById('titleModal'), content=document.getElementById('modalText');
+    fetch('js/dailyData/forms.json')
+        .then(response => response.json())
+        .then(datos => {
+            let correctForm = datos.find(item => item.id === idForm);
+            if(correctForm){
+                document.getElementById('modal').classList.remove('hidden');
+                titulo.innerHTML=correctForm.title;
+                content.innerHTML=correctForm.modalContent;
+            }else{
+                console.log("Error");
+            }
+
+        })
+}
+
+function closeModal() {
+    document.getElementById('modal').classList.add('hidden');
+}
